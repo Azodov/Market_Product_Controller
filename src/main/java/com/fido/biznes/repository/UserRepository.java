d01994f4a40713aefbd87578ac20ed4c318c0030
donik <botirxonn@gmail.com>
2022-08-14 20:47:38 +0500

    deleted not used files and fixed bugs

--- src/main/java/com/fido/biznes/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fido.biznes.repository;
-
-import com.fido.biznes.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
-
